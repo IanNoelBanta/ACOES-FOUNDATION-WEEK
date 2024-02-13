67279261f8e11646c3b11e1e1de0f24806aa5443
IanBanta <ian.noel.banta@adamson.edu.ph>
2024-02-14 00:51:32 +0800

    kilig kilig

--- Hand.py
+++ Hand.py
@@ -1,61 +1,69 @@
+import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import cv2
+import random
 
-# Initialize the webcam to capture video
-# The '2' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
 cap = cv2.VideoCapture(0)
 
-# Initialize the HandDetector class with the given parameters
+heartPNG = cv2.imread("heart2.png",cv2.IMREAD_UNCHANGED)
+timer = 0
+
 detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
 
-# Continuously get frames from the webcam
 while True:
-    # Capture each frame from the webcam
-    # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
+    if timer == 0:
+        dim1 = (random.randrange(0,481), random.randrange(0,640))
+        dim2 = (random.randrange(0,481), random.randrange(0,640))
+        timer = 3
+
     success, img = cap.read()
+    print(img.shape)
 
-    # Find hands in the current frame
-    # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
-    # The 'flipType' parameter flips the image, making it easier for some detections
-    hands, img = detector.findHands(img, draw=True, flipType=True)
+    img = cv2.flip(img, 1)
 
-    # Check if any hands are detected
+    hands, img = detector.findHands(img, draw=False, flipType=False)
+
     if hands:
-        # Information for the first hand detected
-        hand1 = hands[0]  # Get the first hand detected
-        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
-        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
-        center1 = hand1['center']  # Center coordinates of the first hand
-        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
+        hand1 = hands[0]  
+        lmList1 = hand1["lmList"]  
+        bbox1 = hand1["bbox"]  
+        center1 = hand1['center']  
+        handType1 = hand1["type"]  
 
-        # Count the number of fingers up for the first hand
         fingers1 = detector.fingersUp(hand1)
-        print(f'H1 = {fingers1.count(1)}', end=" ")  # Print the count of fingers that are up
 
-        # Calculate distance between specific landmarks on the first hand and draw it on the image
-        length, info, img = detector.findDistance(lmList1[8][0:2], lmList1[12][0:2], img, color=(255, 0, 255),
-                                                  scale=10)
+        length, info, img = detector.findDistance(lmList1[3][0:2], lmList1[7][0:2], img, color=(255, 0, 255),
+                                            scale=10)
+        
+        if length <= 20:
+            cv2.putText(img, "I     CPE!!", (lmList1[3][0]-50, lmList1[3][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            # cv2.putText(img, "I     YOU!!", (lmList2[3][0]-50, lmList2[3][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        # Check if a second hand is detected
+            img = cvzone.overlayPNG(img, heartPNG, pos=dim1)
+
         if len(hands) == 2:
-            # Information for the second hand
             hand2 = hands[1]
             lmList2 = hand2["lmList"]
             bbox2 = hand2["bbox"]
             center2 = hand2['center']
             handType2 = hand2["type"]
 
-            # Count the number of fingers up for the second hand
             fingers2 = detector.fingersUp(hand2)
-            print(f'H2 = {fingers2.count(1)}', end=" ")
 
-            # Calculate distance between the index fingers of both hands and draw it on the image
-            length, info, img = detector.findDistance(lmList1[8][0:2], lmList2[8][0:2], img, color=(255, 0, 0),
+            length, info, img = detector.findDistance(lmList2[3][0:2], lmList2[7][0:2], img, color=(255, 0, 0),
                                                       scale=10)
+            
+            if length <= 20:
+                cv2.putText(img, "I     ENGINEERING!!", (lmList2[3][0]-50, lmList2[3][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                # cv2.putText(img, "I     YOU!!", (lmList2[3][0]-50, lmList2[3][1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        print(" ")  # New line for better readability of the printed output
+                img = cvzone.overlayPNG(img, heartPNG, pos=dim2)
 
-    # Display the image in a window
+
+        print(" ")
+
+    timer -= 1
+
     cv2.imshow("Image", img)
 
     if cv2.waitKey(1) == ord('x'):
